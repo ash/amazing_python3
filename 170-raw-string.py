02989b652714e42d0e01687e3b3b5235296dbdc3
Andrew Shitov <andy@shitov.ru>
2019-05-25 10:10:57 +0200

    25 May

---
+++ 170-raw-string.py
@@ -0,0 +1,13 @@
+# Strings and "raw strings"
+
+# A regular string
+# with an escaped character
+s1 = 'a\tb'
+
+# A raw string
+# \t is not escaped and will
+# appear as is
+s2 = r'a\tb'
+
+print(s1)
+print(s2)
---
+++ 171-long-strings.py
@@ -0,0 +1,14 @@
+# String literal concatenation
+
+# You have a long string and want
+# to put it into a small width of
+# you editor's window
+s = 'Hello, ' 'World!'
+# You can split it in two parts
+print(s)
+
+# Or even into a few lines
+s = 'Hello, ' \
+    'World' \
+    '!'
+print(s)
