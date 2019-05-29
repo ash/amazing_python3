824f9f0a4edae60c4d85ca0dc7ed91e9d8ca3598
Andrew Shitov <andy@shitov.ru>
2019-05-29 22:57:54 +0200

    178

---
+++ 178-iter-dict.py
@@ -0,0 +1,13 @@
+# Iterating over a dictionary is...
+
+data = {
+    "France": "Paris",
+    "Germany": "Berlin",
+    "Italy": "Rome"
+}
+
+for x in data:
+# for x in data.keys(): # the same
+    print(x)
+    # what is x?
+    # these are the keys
