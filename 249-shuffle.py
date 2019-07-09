957b8185070f71990983deccd1149b0adb4c6909
Andrew Shitov <andy@shitov.ru>
2019-07-09 23:41:51 +0200

    9 July

---
+++ 249-shuffle.py
@@ -0,0 +1,9 @@
+# Randomizing lists
+
+import random
+
+data = [10, 20, 30, 40, 50, 60]
+
+random.shuffle(data) # in-place
+
+print(data)
---
+++ 250-inc-all.py
@@ -0,0 +1,8 @@
+# Increment all elements of a list
+
+data = [10, 20, 30, 40, 50, 60]
+
+data = [x + 1 for x in data]
+# this is a list comprehension
+
+print(data)
