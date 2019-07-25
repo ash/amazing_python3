0e93dcc0ea8b66f4fcc8c8965aae571a4648694e
Andrew Shitov <andy@shitov.ru>
2019-07-25 11:00:56 +0200

    270

---
+++ 270.sh
@@ -0,0 +1,14 @@
+# What is the version of current Python?
+
+python3 -V
+#Python 3.8.0b1
+
+# Other Pythons installed here:
+
+python3.6 -V
+#Python 3.6.4
+
+python2.7 -V
+#Python 2.7.16
+
+# Use the -V key to see version number.
