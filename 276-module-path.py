2509cb8d5eb88291aa954ae6042b9dfb238af481
Andrew Shitov <andy@shitov.ru>
2019-07-31 09:38:01 +0200

    276

---
+++ 276-module-path.py
@@ -0,0 +1,10 @@
+# What you can do to see where from
+# the module was loaded
+
+import PIL # this module was
+# installed via pip
+import time # this is a built-in
+# module in Python 3
+
+print(PIL) # you will see the path
+print(time)
