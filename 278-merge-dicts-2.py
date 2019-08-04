4e6aee7ae1dc4a64ea2afc3b80a04cac404d0369
Andrew Shitov <andy@shitov.ru>
2019-08-04 14:45:20 +0300

    fix

--- 278-merge-dicts-2.py
+++ 278-merge-dicts-2.py
@@ -12,7 +12,7 @@ more_capitals = {
 
 capitals = {**capitals, **more_capitals}
 # It is actually equivalent to:
-# capitals = {'France', 'Paris', 'Italy',
-# 'Rome', 'Germany', 'Berlin'}
+# capitals = {'France': 'Paris', 'Italy':
+# 'Rome', 'Germany': 'Berlin'}
 # (google for "kwargs" to see what "**" is)
 print(capitals)
