3cf24e15f8524501f8117ee3347801f73cc7333c
Andrew Shitov <andy@shitov.ru>
2019-08-04 10:58:41 +0300

    278

---
+++ 278-merge-dicts-2.py
@@ -0,0 +1,18 @@
+# How to merge two dictionaries
+# Method 2
+
+capitals = {
+    'France': 'Paris',
+    'Italy': 'Rome',
+}
+
+more_capitals = {
+    'Germany': 'Berlin',
+}
+
+capitals = {**capitals, **more_capitals}
+# It is actually equivalent to:
+# capitals = {'France', 'Paris', 'Italy',
+# 'Rome', 'Germany', 'Berlin'}
+# (google for "kwargs" to see what "**" is)
+print(capitals)
