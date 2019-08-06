85b873593295395c300a5670e56ab6e8bcf2a8d6
Andrew Shitov <andy@shitov.ru>
2019-08-06 09:12:12 +0300

    280

---
+++ 280-set-operations.py
@@ -0,0 +1,12 @@
+# Some operations that you can perform
+# over sets using operators
+
+s1 = {10, 20, 30, 40, 50}
+s2 = {5, 10, 15, 20, 25}
+
+print(s1 - s2) # difference
+print(s2 - s1) # different from s1 - s2
+
+print(s1 & s2) # intersection
+print(s1 | s2) # union
+print(s1 ^ s2) # symmetric difference
