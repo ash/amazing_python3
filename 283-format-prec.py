640607444d9263535e56bdc87307db56822b88ed
Andrew Shitov <andy@shitov.ru>
2019-08-16 08:10:12 +0200

    283

---
+++ 283-format-prec.py
@@ -0,0 +1,12 @@
+# Setting precision when printing
+# numbers using f-strings
+
+import math # to have a number
+
+print(math.pi) # default
+
+print(f'{math.pi}') # again, default
+print(f'{math.pi:.2f}') # two digits
+# after the "."
+print(f'{math.pi:.3f}') 
+print(f'{math.pi:.4f}') 
