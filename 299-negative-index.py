7d9d20349e83207a7949c03dd889db4b65e7a777
Andrew Shitov <andy@shitov.ru>
2019-08-31 13:59:39 +0200

    299

---
+++ 299-negative-index.py
@@ -0,0 +1,16 @@
+# Negative indices when accessing
+# list elements
+
+data = [
+    'alpha', 'beta', 'gamma',
+    'delta', 'epsilon'
+]
+
+# Accessing the first item:
+print(data[0])
+
+# Accessing the last item:
+print(data[-1])
+
+# The second to last
+print(data[-2])
