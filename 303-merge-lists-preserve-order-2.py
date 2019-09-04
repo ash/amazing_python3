d663063cd80ef3989c7494e57af48a78d882aba3
Andrew Shitov <andy@shitov.ru>
2019-09-04 16:36:22 +0200

    303

---
+++ 303-merge-lists-preserve-order-2.py
@@ -0,0 +1,17 @@
+# Merge two lists so that there are
+# no duplicates in the output, and
+# preserve the order of the items
+
+data1 = [3, 5, 7, 9, 11]
+data2 = [5, 8, 10, 11, 12]
+# Both lists contain only unique
+# elements 
+
+# Another solution using list
+# comprehension
+
+data = data1
+data.extend(
+    [x for x in data2 if x not in data1]
+)
+print(data) # merged
