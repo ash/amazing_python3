6a3db949a82093331d6ba6bcc8f6b69f65847a96
Andrew Shitov <andy@shitov.ru>
2019-09-21 16:32:37 +0200

    "jobs" list is not needed

--- 323-thread-execution.py
+++ 323-thread-execution.py
@@ -8,11 +8,10 @@ def do_work(x):
     time.sleep(0.1)
     print(x)
 
-jobs = []
 n_jobs = 10 # number of threads
 with ThreadPoolExecutor(max_workers=n_jobs) as e:
     for x in range(n_jobs):
-        jobs.append(e.submit(do_work, x))
+        e.submit(do_work, x)
         # submit a job to execute a function
         # with an argument (x)
 # All jobs submitted
