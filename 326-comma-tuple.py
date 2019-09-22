6bf91a323ad0b6ef61d1a39eb2d4dc4f8bbb205a
Andrew Shitov <andy@shitov.ru>
2019-09-22 15:35:05 +0200

    326

---
+++ 326-comma-tuple.py
@@ -0,0 +1,11 @@
+# Using a trailing comma to
+# create a single-element tuple
+
+a = (42) # this is an int
+print(type(a))
+print(a)
+
+b = (42,) # this is a tuple
+# with only one element in it
+print(type(b))
+print(b)
