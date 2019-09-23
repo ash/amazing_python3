14a3e3189b1af6be68566f78f3f2092170f21426
Andrew Shitov <andy@shitov.ru>
2019-09-23 10:13:28 +0200

    328

---
+++ 328-create-tuple.py
@@ -0,0 +1,13 @@
+# Ways to create a tuple
+
+t1 = 10, 20, 30
+t2 = (10, 20, 30)
+t3 = 10, # single element in a tuple
+t4 = (10,)
+t5 = tuple([10, 20, 30])
+
+print(type(t1))
+print(type(t2))
+print(type(t3))
+print(type(t4))
+print(type(t5))
