3f5777c6fc1a1f62ae7e2b1a452796f4001af089
Andrew Shitov <andy@shitov.ru>
2019-12-29 20:13:02 +0100

    Python2 to 3 recipe 1

---
+++ migrating-to-python3/migrate1.py
@@ -0,0 +1,15 @@
+# In Python 2, "print" is a statement.
+# In Python 3, "print" is a function.
+# You need to have parentheses.
+
+# Python 2:
+# print "Hello, World!"
+
+# x = 42
+# print "The answer is", x
+
+# Python 3:
+print("Hello, World!")
+
+x = 42
+print("The answer is", x)
