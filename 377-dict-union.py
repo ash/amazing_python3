9db785e5ab378257dcb2ed94c88c8d058063dbbe
Andrew Shitov <andy@shitov.ru>
2020-10-28 09:34:14 +0100

    377 merge dicts with |

---
+++ 377-dict-union.py
@@ -0,0 +1,11 @@
+# Set operations with 
+# dictionaries. Python 3.9+
+
+data1 = {"alpha": 10, "beta": 20}
+data2 = {"gamma": 30}
+
+# Let's merge the two dicts
+data = data1 | data2
+#           ^^^
+
+print(data)
