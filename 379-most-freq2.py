cf4002824e2c96b0575708973ac074fe396c7ac3
Andrew Shitov <andy@shitov.ru>
2020-10-29 16:56:05 +0100

    379 statistics.mode

---
+++ 379-most-freq2.py
@@ -0,0 +1,10 @@
+# Find the most frequent item
+# in a list (of integers here)
+
+# Method 2. Using modules
+import statistics
+
+data = [10, 20, 30, 10, 20, 40, 20]
+# The answer is 20
+
+print(statistics.mode(data))
