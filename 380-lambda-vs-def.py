5b497839c2a14c7c0c3772bb792d8be29190f398
Andrew Shitov <andy@shitov.ru>
2020-10-30 12:13:40 +0100

    380 lambda vs def

---
+++ 380-lambda-vs-def.py
@@ -0,0 +1,14 @@
+# Simple functions can be created
+# in just a single line of code.
+# Compare:
+
+# Standard function
+def add(x, y):
+    return x + y
+
+# Lambda function
+add1 = lambda x, y: x + y
+
+# Let's use both:
+print(add(10, 20))
+print(add1(10, 20))
