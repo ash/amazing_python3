239e43cec5dd3f96dece5ea8cb54ae749aeb4ec7
Andrew Shitov <andy@shitov.ru>
2020-11-05 13:28:50 +0100

    Task 002 fizz-buzz

---
+++ tasks/t-002.py
@@ -0,0 +1,13 @@
+# The program prints ‘fizz’ if the
+# number is divisible by 3 and ‘buzz’
+# if it is divisible by 5.
+
+for i in range(5, 20):
+    print(i, end=': ')
+    if not(i % 3):
+        print('fizz', end='')
+    if not(i % 5):
+        print('buzz', end='')
+    print('') # newline actually
+
+    
