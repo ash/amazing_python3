e0dd33b1782e9c604f20e4be2e12f64b435e2556
Andrew Shitov <andy@shitov.ru>
2020-11-11 15:59:30 +0100

    task 8; division via subtraction

---
+++ tasks/t-008.py
@@ -0,0 +1,14 @@
+# Integer division withou division
+m = 112
+n = 26
+
+print('Must be:', m // n)
+
+# Using subtraction instead
+r = 0
+while m >= n:
+    m -= n
+    r += 1
+
+# Same result as above
+print(r)
