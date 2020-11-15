82fee0500d12a3de15c2ad6acc6cbe43eb170391
Andrew Shitov <andy@shitov.ru>
2020-11-15 13:42:33 +0100

    task 12: reverse int

--- tasks/t-012.py
+++ tasks/t-012.py
@@ -1,11 +1,10 @@
 import sys
 
-x1, y1, x2, y2, x3, y3, x4, y4 = sys.argv[1:]
+n = int(sys.argv[1])
 
-# Make sure x1 is always smaller than x2
-if x2 > x1:
-    x1, x2 = x2, x1
+sign = 1 if n > 0 else -1
+str_n = str(abs(n))
+str_n = str_n[::-1]
 
-if y2 > x1:
-    x1, x2 = x2, x1
-
+r = sign * int(str_n)
+print(r)
