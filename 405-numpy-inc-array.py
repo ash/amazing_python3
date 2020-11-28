211ef7672820d4a4baae022040b2ec95a31386f1
Andrew Shitov <andy@shitov.ru>
2020-11-28 09:37:48 +0100

    405 numpy inc array

---
+++ 405-numpy-inc-array.py
@@ -0,0 +1,12 @@
+# Incrementing ALL items of a list.
+# Let us use numpy.
+
+import numpy
+
+a = numpy.array([10, 20, 30, 40])
+
+# Let us make it [11, 21, 31, 41]
+
+a += 1
+
+print(a) # [11 21 31 41]
