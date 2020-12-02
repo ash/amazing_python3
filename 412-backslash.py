d022d456afeb39797aa63ee7da0d5abf073f67a0
Andrew Shitov <andy@shitov.ru>
2020-12-02 11:21:06 +0100

    412 backslash

---
+++ 412-backslash.py
@@ -0,0 +1,15 @@
+# What is \ in Python?
+# Illustrating this on a simple
+# program
+data = "1,3,5,7,9,11"
+for item in \
+    data.split(','):
+    print(item)
+
+# What if I want to split actions
+# into separate lines to show the
+# structure more clearly?
+
+# Add a backslash to tell Python that
+# you are going to continue that line
+# on the next line :)
